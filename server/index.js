5010334547f1e0d918270506b70daf5e866bc427
vishal <vishalgoud890@gmail.com>
2023-08-14 23:36:33 +0530

    intial commit setAvatar not functioning

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+const userRoutes = require("./routes/userRoutes")
+const mongoose = require("mongoose")
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",userRoutes)
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+console.log(err)
+})
+
+const server = app.listen(process.env.PORT,()=>{
+console.log(`Server is running on port ${process.env.port}`);
+})
